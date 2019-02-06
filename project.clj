2db7b5c1559b42af151b6fc191ea75e18cf05f51
Max <weber.maximilian@googlemail.com>
2019-02-06 08:59:08 +0100

    Updated dependencies.

--- project.clj
+++ project.clj
@@ -4,11 +4,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[base64-clj "0.1.1"]
-                 [cheshire "5.4.0"]
+                 [cheshire "5.8.1"]
                  [crypto-equality "1.0.0"]
                  [crypto-random "1.2.0"]
-                 [org.clojure/clojure "1.6.0"]
-                 [pandect "0.4.1"]]
+                 [org.clojure/clojure "1.10.0"]
+                 [pandect "0.6.1"]
+                 [org.bouncycastle/bcprov-jdk15on "1.60"]]
   :plugins [[codox "0.8.10"]]
   :codox {:src-dir-uri "https://github.com/mkwiatkowski/rails-session-clojure/blob/master/"
           :src-linenum-anchor-prefix "L"
